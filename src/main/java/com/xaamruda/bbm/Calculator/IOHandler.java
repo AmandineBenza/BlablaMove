4c1954d5952d147d50dace5aeb786581a6246254
MEERSMAN Rudy <33250028+Rmeersman@users.noreply.github.com>
2018-10-17 10:35:44 +0200

    #5 Billing skeleton
    Add of Billing Skeleton with a simple calcul

---
+++ src/main/java/com/xaamruda/bbm/Calculator/IOHandler.java
@@ -0,0 +1,12 @@
+package com.xaamruda.bbm.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+/**
+ * Calculator entry point.
+ */
+public class IOHandler{
+
+}
---
+++ src/main/java/com/xaamruda/bbm/Calculator/MainCalculator/ICalculator.java
@@ -0,0 +1,10 @@
+package com.xaamruda.bbm.calculator;
+
+/**
+ * External provider connector.
+ */
+public interface ICalculator{
+
+	public Calculator simpleCalcul(int weight,int size, int distance,int day);
+
+}
