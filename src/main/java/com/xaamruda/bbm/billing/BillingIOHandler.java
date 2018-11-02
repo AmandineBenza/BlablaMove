4091c0671a5c4bb8e64bada5bdcbe1641033f3b4
Rmeersman <roody06@hotmail.fr>
2018-11-02 10:26:45 +0100

    Update Calculator
    update the calculator handler

--- src/main/java/com/xaamruda/bbm/billing/BillingIOHandler.java
+++ src/main/java/com/xaamruda/bbm/billing/BillingIOHandler.java
@@ -1,12 +1,16 @@
 package com.xaamruda.bbm.billing;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonObject;
+import com.xaamruda.bbm.commons.json.JsonUtils;
+import com.xaamruda.bbm.offers.model.OffersTransaction;
 import org.springframework.stereotype.Component;
 
-import com.google.gson.Gson;
 import com.xaamruda.bbm.billing.calculator.Calculator;
 import com.xaamruda.bbm.offers.model.Offer;
-import com.xaamruda.bbm.offers.model.OffersTransaction;
-import com.xaamruda.bbm.offers.model.PostedOffer;
 
 /**
  * Class for Calculator entry point.
@@ -16,50 +20,43 @@ public class BillingIOHandler {
 	Calculator facturation = new Calculator();
 
 	/**
-	 * calcul point repartition with an ....
-	 * @param workData string of the object user
+	 * do work depending of event
+	 * @param workData Json containing data for the calculator
 	 * @return the calculation of point for user
 	 */
-	//TODO distance
-	public String calcul_without_offer(String workData) {
-		OffersTransaction offer = workData_Parser(workData);
-		facturation.calcul_price_base(offer.getWeigth(),0,offer.getVolume(),offer.getDateBeforeOrder());
-		offer.setFinalPrice((int)facturation.getUserPoint());
-		return reponse_flow_parser(offer);
+	public String mainHandler(String workData){
+		JsonObject data = JsonUtils.getFromJson(workData);
+		if(data.get("event").equals("calcul_without_offer")){
+			return calcul_without_offer(data);
+		}else if (JsonUtils.getFromJson(workData).get("event").equals("calcul_with_offer")){
+			return calcul_with_offer(data);
+		}else{
+			return "error : Wrong event";
+		}
+	}
+	/**
+	 * calcul point repartition with an ....
+	 * @param workData Json containing data for the calculator
+	 * @return the calculation of point for user
+	 */
+	private String calcul_without_offer(JsonObject workData) {
+		JsonObject data = workData.get("data").getAsJsonObject();
+		facturation.calcul_price_base(data.get("weight").getAsInt(),
+				data.get("distance").getAsInt(),
+				data.get("volume").getAsInt(),
+				data.get("date").getAsInt());
+		return JsonUtils.toJson("{ userPoint : " + facturation.getUserPoint() + ", socityPoint : " + facturation.getSocityPoint());
 	}
 
 	/**
 	 * calcul point repartition with an offer
-	 * @param workData string of the object user
-	 * @param offer object for offer
+	 * @param workData Json containing data for the calculator
 	 * @return the calculation of point for user
 	 */
-	public String calcul_with_offer(String workData, PostedOffer offer) {
-		OffersTransaction user = workData_Parser(workData);
-		facturation.advance_date_with_offer(offer.getPrice(),user.getFinalPrice());
-		user.setFinalPrice((int) facturation.getUserPoint());
-		return reponse_flow_parser(offer);
-	}
-	/**
-	 * parse the response that will be send
-	 * @param type object we want to parse in the response
-	 * @return String of the object
-	 */
-	public String reponse_flow_parser(Object type) {
-		String res = "";
-		Gson gson = new Gson();
-		res = gson.toJson(type);
-		return res;
-	}
-
-	/**
-	 * parser to converst string to the object user
-	 * @param workData string of the object user
-	 * @return the object user
-	 */
-	private OffersTransaction workData_Parser(String workData) {
-		Gson gson = new Gson();
-		OffersTransaction res = gson.fromJson(workData, OffersTransaction.class);
-		return res;
+	private String calcul_with_offer(JsonObject workData) {
+		JsonObject data = workData.get("data").getAsJsonObject();
+		facturation.advance_date_with_offer(data.get("date").getAsInt(),
+				data.get("offerPrice").getAsInt());
+		return JsonUtils.toJson("{ userPoint : " + facturation.getUserPoint() + ", socityPoint : " + facturation.getSocityPoint());
 	}
 }
