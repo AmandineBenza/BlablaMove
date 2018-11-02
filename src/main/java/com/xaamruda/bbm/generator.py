41dabf54efd326e7298db3ab517049a50e0e9f42
Rmeersman <roody06@hotmail.fr>
2018-11-02 10:11:13 +0100

    Update Generator
    Update of the user's Request generator

--- src/main/java/com/xaamruda/bbm/generator.py
+++ src/main/java/com/xaamruda/bbm/generator.py
@@ -1,16 +1,32 @@
 import sys
 
 def parser_CreateUser(argument):
-    return "{'event':'create-user','data':{'mail':" + argument[1] + ",'name':" + argument[2] + ",'address':"+ argument[3] +",'phone':"+ argument[4] + ",'password':" + argument[5] + "}}"
+    return "\"{'event':'create-user','data':{'mail':" + argument[1] + ",'name':" + argument[2] + ",'address':"+ argument[3] +",'phone':"+ argument[4] + ",'password':" + argument[5] + "}}\" 'localhost:8080/BBM/USERS/'"
 
 def parser_CreateOffer(argument):
-    return ""
+    return "\"{'event':'create-offer','data':{"+ " " + "}}\"  'localhost:8080/BBM/USERS/'"
+
+def parser_IdentifyUser(argument):
+    return "\"{'event':'identify-user','data':{"+ " " + "}}\"  'localhost:8080/BBM/USERS/'"
+
+def parser_ConsultUser(argument):
+    return "\"{'event':'consult-users','data':{'mail':" + argument[1] + "}}\" 'localhost:8080/BBM/USERS/' "
+
+def parser_ConsultOffer(argument):
+    return  "\"{'event':'consult-offer','data':{"+ " " + "}}\"  'localhost:8080/BBM/USERS/'"
+
 def main(argument):
     header = "curl -H 'Accept: application/json' -H 'Content-type: application/json' -X POST -d "
     if(argument[0] == "create-user"):
         return header + parser_CreateUser(argument)
     elif(argument[0] == "create-offer"):
         return header + parser_CreateOffer(argument)
+    elif(argument[0] == "consult-offer"):
+        return header + parser_ConsultOffer(argument)
+    elif(argument[0] == "identify-user"):
+        return header + parser_IdentifyUser(argument)
+    elif(argument[0] == "consult-user"):
+        return header + parser_ConsultUser(argument)
     else:
         return "Wrong event"
 
