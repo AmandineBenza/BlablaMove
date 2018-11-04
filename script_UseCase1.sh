b71e2ead900039ed776bb65d7c98f5c011d42dca
MEERSMAN Rudy <33250028+Rmeersman@users.noreply.github.com>
2018-11-04 19:10:46 +0100

    Add Script
    Add a base for Poc Script

---
+++ script_UseCase1.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+echo "1.Bob is a lambda student who wants to move."
+echo ""
+
+sleep $*;
+echo "2.Bob need to transport his bed from his parents house(Nice) to is new student apartment (Sophia)."
+echo ""
+
+sleep $*;
+echo "3.Bob is a smart guy so he decide to use BlablaMove."
+echo ""
+
+sleep $*;
+echo "4.He Login on BlablaMove: he has the right amount of points."
+echo ""
+
+sleep $*;
+echo "5.He fill a form where he give some informations: the start point of the things he want to move, the arrival point, the size of his bed, the weight of his bed, when he wants to move (range ?) and the maximum number of points he want to spend."
+echo ""
+
+sleep $*;
+echo "6.The system give him a list of results who answer his need. (Number of points/Date/Hours/....)"
+echo ""
+
+sleep $*;
+echo "7.Bob chose a ride for his bed."
+echo ""
+
+sleep $*;
+echo "8.The system answer him with a recap."
+echo ""
+
+sleep $*;
+echo "9.Bob confirm."
+echo ""
+
+sleep $*;
+echo "10.He receive a confirmation mail from BlablaMove : Charlie can help him to move his things."
+echo ""
+
+sleep $*;
+echo "11.---Ellipse---"
+echo ""
+
+sleep $*;
+echo "12.At the chosen date, Charlie goes to Bob house and take his bed."
+echo ""
+
+sleep $*;
+echo "13.Charlie goes to Sophia."
+echo ""
+
+sleep $*;
+echo "14.Bob receive an notification that confirm the delivery of his bed."
+echo ""
+
+sleep $*;
+echo "15.Bob can now confirm the transaction to BlablaMove."
+echo ""
+
+sleep $*;
+echo "16.After the confirmation from Bob,the BalblaMove collects the points that were needed for this transaction."
