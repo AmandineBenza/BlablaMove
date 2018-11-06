421e1f06e15e52b16bc54ecbf2af39f922e344a6
MEERSMAN Rudy <33250028+Rmeersman@users.noreply.github.com>
2018-11-06 17:44:35 +0100

    update script
    update script useCase1 with some curl

--- src/main/resources/requests/script_UseCase1.sh
+++ src/main/resources/requests/script_UseCase1.sh
@@ -17,6 +17,7 @@ echo ""
 sleep $*;
 echo "5.He fill a form where he give some informations: the start point of the things he want to move, the arrival point, the size of his bed, the weight of his bed, when he wants to move (range ?) and the maximum number of points he want to spend."
 echo ""
+curl -H "Accept: application/json" -H "Content-type: application /json" -X POST -d
 
 sleep $*;
 echo "6.The system give him a list of results who answer his need. (Number of points/Date/Hours/....)"
@@ -25,14 +26,17 @@ echo ""
 sleep $*;
 echo "7.Bob chose a ride for his bed."
 echo ""
+curl -H "Accept: application/json" -H "Content-type: application /json" -X POST -d
 
 sleep $*;
 echo "8.The system answer him with a recap."
 echo ""
 
+
 sleep $*;
 echo "9.Bob confirm."
 echo ""
+curl -H "Accept: application/json" -H "Content-type: application /json" -X POST -d
 
 sleep $*;
 echo "10.He receive a confirmation mail from BlablaMove : Charlie can help him to move his things."
@@ -53,10 +57,12 @@ echo ""
 sleep $*;
 echo "14.Bob receive an notification that confirm the delivery of his bed."
 echo ""
+curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d
 
 sleep $*;
 echo "15.Bob can now confirm the transaction to BlablaMove."
 echo ""
+curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d
 
 sleep $*;
 echo "16.After the confirmation from Bob,the BalblaMove collects the points that were needed for this transaction."
