459917791c51dd0c51275d569881a60a43479363
FORNALI Damien <itsdamoy@gmail.com>
2018-11-08 23:07:05 +0100

    log

--- src/main/resources/requests/run.sh
+++ src/main/resources/requests/run.sh
@@ -22,11 +22,7 @@ echo "\t-arrival location"
 echo "\t-price"
 echo "\t-capacity of car\n"
 
-<<<<<<< HEAD
 priceRequest=$(curl -s -H "Accept: application/json" -H "Content-type: application/json" -X POST -d "{\"event\" : \"validate-price\" , \"data\" : {\"data\" : \"x\"}, \"filters\": {\"startAddress\": \"$startAdress\",\"endAddress\": \"$endAddress\",\"maxPrice\": \"0\"}}" "localhost:8080/BBM/OFFERS/" | grep -o -P 'F.*' );
-=======
-priceRequest=$(curl -s -H "Accept: application/json" -H "Content-type: application/json" -X POST -d "{\"event\" : \"validate-price\" , \"data\" : {\"data\" : \"x\"}, \"filters\": {\"startAddress\": \"$startAdress\",\"endAddress\": \"$endAddress\",\"maxPrice\": \"0\"}}" "localhost:8080/BBM/OFFERS/");		
->>>>>>> branch 'master' of https://github.com/Damoy/BlablaMove.git
 echo $priceRequest
 
 rangePrice=$(echo $priceRequest | grep -o -P '\[\d* : \d*\]')
