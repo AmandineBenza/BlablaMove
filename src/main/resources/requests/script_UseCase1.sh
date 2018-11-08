0339243753db69d689c1dd5a72dbd14f19a14133
MEERSMAN Rudy <33250028+Rmeersman@users.noreply.github.com>
2018-11-08 16:17:38 +0100

    update script
    update script for POC

--- src/main/resources/requests/script_UseCase1.sh
+++ src/main/resources/requests/script_UseCase1.sh
@@ -20,17 +20,12 @@ curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -
 
 printf ">> Starting BlablaMove scenario"; sleep 1; printf "."; sleep 1; printf "."; sleep 1; printf ".\n"; echo "";
 
-echo "A.1. Alice is a student who lives in Nice and goes to Sophia in car every day for her studies.\n"
-sleep $*;
-
-echo "A.2. On BlablaMove she created one offer to transport things between Nice and Sophia every day, between 7:30 am (Nice) and 8:30 am (Sophia)."
+echo "A.1. On BlablaMove she created one offer to transport things between Nice and Sophia every day, between 7:30 am (Nice) and 8:30 am (Sophia)."
 echo "\t-start location"
 echo "\t-arrival location"
 echo "\t-price"
 echo "\t-capacity of car"
 echo ""
-
-
 
 priceRequest=$(curl -s -H "Accept: application/json" -H "Content-type: application/json" -X POST -d "{\"event\" : \"validate-price\" , \"data\" : {\"data\" : \"x\"}, \"filters\": {\"startAddress\": \"$startAdress\",\"endAddress\": \"$endAddress\",\"maxPrice\": \"0\"}}" "localhost:8080/BBM/OFFERS/")
 echo $priceRequest
@@ -41,24 +36,7 @@ minPrice=$(echo $rangePrice | grep -o -P '\[\d*' | grep -o -P '\d*')
 
 curl -s -H "Accept: application/json" -H "Content-type: application/json" -X POST -d "{\"event\":\"create-offer\",\"data\":{\"ownerID\":\"$driver\", \"price\": \"$(echo $minPrice*1.1 | bc | cut -f1 -d\.)\", \"startCity\":\"$startAdress\", \"endCity\":\"$endAdress\", \"capacity\":\"$carV\" }}" "localhost:8080/BBM/OFFERS"
 
-
-echo "A.3. When she makes the offer, BlablaMove suggests nn amount of point she should charge for the delivery. Based on the number of points that are usually charged for this distance.\n"
-
-sleep $*;
-echo "A.4. She can choose to charge the amount BlablaMove suggest her, or she can make a new offer. The number of points she can charge for a delivery will be in a certain range proposed by the system, it can't be too expensive compare to the average offers.\n"
-
-sleep $*;
-echo "B.1. Bob is a lambda student who wants to move.\n"
-sleep $*;
-
-sleep $*;
-echo "B.2. Bob needs to transport his bed from his parents house (Nice) to his new student apartment (Sophia).\n"
-
-sleep $*;
-echo "B.3. Bob is a smart guy: he decides to use BlablaMove.\n"
-
-sleep $*;
-echo "B.4. He Logins on BlablaMove: he has the right amount of points."
+echo "B.1. Bob Logins on BlablaMove: he has the right amount of points."
 sleep 0.5; printf "Processing Bob login"; sleep 1; printf "."; sleep 1; printf "."; sleep 1; printf ".\n";
 echo ""
 
@@ -67,7 +45,7 @@ curl -s -H "Accept: application/json" -H "Content-type: application/json" -X POS
 echo "\" \""
  
 sleep $*;
-echo "B.5. He fills a form with following information:"
+echo "B.2. He fills a form with following information:"
 echo "\t-start location"
 echo "\t-arrival location"
 echo "\t-bed's size and weight"
@@ -79,7 +57,7 @@ echo ""
 
 
 sleep $*;
-echo "B.6. The system proposes a list of results that match his requirements."
+echo "B.3. The system proposes a list of results that match his requirements."
 sleep 0.5; printf "Retrieving relevant offers"; sleep 1; printf "."; sleep 1; printf "."; sleep 1; printf ".\n";
 echo ""
 
@@ -90,7 +68,7 @@ echo $searchResultList
 firstResult=$(echo $searchResultList | jq '.[0]')
 
 sleep $*;
-echo "B.7. Bob choses a ride for his bed.\n"
+echo "B.4. Bob choses a ride for his bed and confirm.\n"
 
 oId=$(echo $firstResult | jq '.offerID')
 
@@ -100,14 +78,15 @@ echo $askValue
 
 
 sleep $*;
-echo "B.8. The system answers with a recap.\n"
+echo "B.5. The system answers with a recap.\n"
+
+price=$(echo $askValue | jq '.finalPrice')
+recap=$(curl -s -H "Accept: application/json" -H "Content-type: application/json" -X POST -d "{\"event\":\"confirm-command\" ,\"data\": {\"offerID\": $oId,\"date\":\"$inDays\", \"startAddress\":\"$startAdress\", \"endAddress\":\"$endAdress\",\"price\":\"$price\" }}" "localhost:8080/BBM/OFFERS")
+
 
 sleep $*;
-echo "B.9. Bob confirms.\n"
-# curl -s-H "Accept: application/json" -H "Content-type: application /json" -X POST -d
 
-sleep $*;
-echo "A.5. One day she receives a mail form BlablaMove : Dimitri wants her to transport a box from Nice to Sophia at a certain date.\n"
+echo "A.2. One day she receives a mail form BlablaMove : Bob wants her to transport a box from Nice to Sophia at a certain date.\n"
 
 offersIdDriver=$(curl -s -H "Accept: application/json" -H "Content-type: application/json" -X POST -d "{\"event\": \"consult-awaiting-offers\" ,\"data\": {\"ownerID\": \"$driver\"}}" "localhost:8080/BBM/OFFERS")
 
@@ -115,19 +94,13 @@ echo $offersIdDriver
 offerIdD=$(echo $offersIdDriver | jq '.[0].transactionID')
 
 sleep $*;
-echo "A.6. She agrees to do it and confirm on BlablaMove.\n"
+echo "A.3. She agrees to do it and confirm on BlablaMove.\n"
 
 curl -s -H "Accept: application/json" -H "Content-type: application/json" -X POST -d "{\"event\": \"confirm-awaiting-offers\" ,\"data\": {\"transactionID\": $offerIdD}}" "localhost:8080/BBM/OFFERS"
 
 
 sleep $*;
-echo "B.10. He receives a confirmation mail from BlablaMove : Charlie can help him to move his things.\n"
 
-
-sleep $*;
-echo ">> Ellipse !\n"
-
-sleep $*;
 echo "At the chosen date, Alice goes to Bob house and takes his bed.\n"
 
 
@@ -139,27 +112,15 @@ curl -s -H "Accept: application/json" -H "Content-type: application/json" -X POS
 curl -s -H "Accept: application/json" -H "Content-type: application/json" -X POST -d "{\"event\": \"confirm-receipt\" ,\"data\": {\"transactionID\": $offerIdD}}" "localhost:8080/BBM/OFFERS"
 
 
-echo "B.11. Alice goes to Sophia with Bob and leave him and his box at the right place.\n"
-sleep $*;
-
-echo "A.7 She confirms on BlablaMove that she delivered the box.\n"
+echo "A.4. Alice confirms on BlablaMove that she delivered the box.\n"
 
 curl -s -H "Accept: application/json" -H "Content-type: application/json" -X POST -d "{\"event\": \"claim-deposit\" ,\"data\": {\"transactionID\": $offerIdD}}" "localhost:8080/BBM/OFFERS"
 
 sleep $*;
-echo "B.12. Bob receives a notification that confirms the delivery of his bed.\n"
-# curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"event": "claim-deposit" ,"data": {"transactionID": "$transactionID"}}' "localhost:8080/BBM/OFFERS"
 
-sleep $*;
-
-echo "B.13. Bob can now confirm the transaction to BlablaMove.\n"
+echo "B.6. Bob can now confirm the transaction to BlablaMove.\n"
 curl -s -H "Accept: application/json" -H "Content-type: application/json" -X POST -d "{\"event\": \"confirm-deposit\" ,\"data\": {\"transactionID\": $transactionID}}" "localhost:8080/BBM/OFFERS"
 
-echo "16. Finally, after Bob's confirmation, BlablaMove performs a points transaction.\n"
-sleep $*
-
-echo "A.8. Alice receives points for the delivery."
-#curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"event": "confirm-deposit" ,"data": {"transactionID": "$transactionID"}}' "localhost:8080/BBM/OFFERS"
 sleep $*;
 
 echo ">> End of scenario !"
