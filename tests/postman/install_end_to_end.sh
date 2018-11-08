944e864561ee86cc2d31bb54187aedb6c243d216
FORNALI Damien <itsdamoy@gmail.com>
2018-11-08 14:59:39 +0100

    #11  Postman testing

--- tests/postman/install_end_to_end.sh
+++ tests/postman/install_end_to_end.sh
@@ -1,15 +1,31 @@
 #!/bin/sh
 
 echo "---- [WARNING] Requires Linux ---- ";
-echo "---- Postman end-to-end testing. Pre-installation ----";
+echo "---- Postman end-to-end testing. Required installation ----";
+echo ">> Updating packages...";
+sleep 1;
+sudo apt update;
+echo ">> Done."
+sleep 1;
+echo ">> Installing nodejs...";
+sleep 1;
+sudo apt install nodejs npm;
+sleep 1;
+sudo apt install nodejs-legacy;
+sleep 3;
+echo ">> Done.";
+sleep 1;
 echo ">> Installing npm...";
 sleep 1;
 sudo apt-get install npm;
+sleep 1;
+sudo npm install -g n;
+sudo n latest;
 sleep 3;
 echo ">> Done."
-sleep 2.
+sleep 2;
 echo ">> Installing newman...";
-sudo /usr/bin/npm install -g newman
+sudo npm install -g newman
 sleep 3;
 echo ">> Done."
 sleep 1;
