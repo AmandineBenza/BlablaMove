34171330e5e3cccfd2413597d4d7583b2dc21bc3
FORNALI Damien <itsdamoy@gmail.com>
2019-01-12 14:54:14 +0100

    Refactor

--- src/main/java/com/xaamruda/bbm/offers/dbaccess/repository/IOffersTransactionRepository.java
+++ src/main/java/com/xaamruda/bbm/offers/dbaccess/repository/IOffersTransactionRepository.java
@@ -3,16 +3,13 @@ package com.xaamruda.bbm.offers.dbaccess.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
 import com.xaamruda.bbm.offers.model.OffersTransaction;
-import com.xaamruda.bbm.offers.model.PostedOffer;
 
-public interface IOffersTransactionRepository extends JpaRepository<OffersTransaction, Integer>//CrudRepository<ClientOffer, Integer> 
-{
- 
+public interface IOffersTransactionRepository extends JpaRepository<OffersTransaction, Integer>{
+
 	List<OffersTransaction> getByOwnerID(String ownerID);
 
 	List<OffersTransaction> getByTransactionID(Integer transactionID);
-	
+
 }
