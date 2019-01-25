d1b87ad6f225d59ccf9de521a504ad8bc5c13620
FORNALI Damien <itsdamoy@gmail.com>
2019-01-25 09:47:47 +0100

    Fix mockito dependencyy and test identify

--- src/test/java/com/xaamruda/bbm/users/TestUsersIOHandler.java
+++ src/test/java/com/xaamruda/bbm/users/TestUsersIOHandler.java
@@ -1,62 +1,86 @@
 package com.xaamruda.bbm.users;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.internal.util.reflection.Whitebox;
-import org.powermock.api.support.membermodification.MemberModifier;
+import org.mockito.junit.MockitoJUnitRunner;
 
-import com.xaamruda.bbm.users.dbaccess.service.UserService;
-import com.xaamruda.bbm.users.identification.UserIdentificator;
-import com.xaamruda.bbm.users.info.UserDataManager;
+import com.xaamruda.bbm.users.dbaccess.service.IUserService;
+import com.xaamruda.bbm.users.identification.IUserIdentificator;
+import com.xaamruda.bbm.users.info.IUserDataManager;
 import com.xaamruda.bbm.users.model.User;
 
+@RunWith(MockitoJUnitRunner.class)
 public class TestUsersIOHandler {
 
-	@Mock
 	private UsersIOHandler usersIOHandler;
 	
 	@Mock
-	private UserService service;
+	private IUserService service;
 	
 	@Mock
-	private UserDataManager dataManager;
+	private IUserDataManager dataManager;
 	
 	@Mock
-	private UserIdentificator identificator;
+	private IUserIdentificator identificator;
 	
 	@Before
 	@Test
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
+		usersIOHandler = new UsersIOHandler();
+		
 		Whitebox.setInternalState(usersIOHandler, "service", service);
 		Whitebox.setInternalState(usersIOHandler, "identificator", identificator);
 		Whitebox.setInternalState(usersIOHandler, "dataManager", dataManager);
+
+		assertNotNull(service);
+		assertNotNull(identificator);
+		assertNotNull(dataManager);
 		
-		// assertEquals(usersIOHandler.get, actual);
+		assertEquals(Whitebox.getInternalState(usersIOHandler, "service"), service);
+		assertEquals(Whitebox.getInternalState(usersIOHandler, "identificator"), identificator);
+		assertEquals(Whitebox.getInternalState(usersIOHandler, "dataManager"), dataManager);
 	}
 	
-	//@Test
+	@Test
 	public void testIdentifyUserByMailPlusPassword_success() {
-		// setup
-		Mockito.when(identificator.identify(Mockito.anyString(), Mockito.anyString())).thenReturn(true);
-		
+		List<User> users = new ArrayList<>();
 		User user = new User();
 		user.setIdentified(false);
-		user.setId(0);
+		users.add(user);
+		
+		Mockito.when(identificator.identify(Mockito.anyString(), Mockito.anyString())).thenReturn(true);
+		Mockito.when(service.getUserByMail(Mockito.anyString())).thenReturn(users);
 		
 		boolean result = usersIOHandler.identifyUserByMailPlusPassword("test", "test");
 		assertEquals(result, user.isIdentified());
-		assertEquals(user.isIdentified(), true);
+		assertTrue(user.isIdentified());
 	}
 	
 	@Test
 	public void testIdentifyUserByMailPlusPassword_fail() {
+		List<User> users = new ArrayList<>();
+		User user = new User();
+		user.setIdentified(false);
+		users.add(user);
 		
+		Mockito.when(identificator.identify(Mockito.anyString(), Mockito.anyString())).thenReturn(false);
+		
+		boolean result = usersIOHandler.identifyUserByMailPlusPassword("test", "test");
+		assertEquals(result, user.isIdentified());
+		assertEquals(user.isIdentified(), false);
 	}
 	
 }
