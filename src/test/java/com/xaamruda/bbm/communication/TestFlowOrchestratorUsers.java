7321d1aa410015c1ef34f2abad228bd5ac4ee207
Amandine Benza <amandine.benza@etu.unice.fr>
2019-01-27 11:48:00 +0100

    #22 Flow Orchestrator - Test Users Entry points

--- src/test/java/com/xaamruda/bbm/communication/TestFlowOrchestratorUsers.java
+++ src/test/java/com/xaamruda/bbm/communication/TestFlowOrchestratorUsers.java
@@ -1,6 +1,10 @@
 package com.xaamruda.bbm.communication;
 
+import static java.util.Collections.singletonList;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -12,22 +16,14 @@ import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.HttpStatus;
 
 
-import com.xaamruda.bbm.billing.BillingIOHandler;
 import com.xaamruda.bbm.communication.internal.FlowOrchestrationResult;
 import com.xaamruda.bbm.communication.internal.FlowOrchestrator;
-import com.xaamruda.bbm.integrity.IntegrityIOHandler;
 import com.xaamruda.bbm.offers.OffersIOHandler;
-import com.xaamruda.bbm.roads.RoadsIOHandler;
 import com.xaamruda.bbm.users.UsersIOHandler;
 import com.xaamruda.bbm.users.model.User;
-import static java.util.Collections.singletonList;
-
-import static org.mockito.Mockito.when;
-
-import java.util.List;
 
 @RunWith(MockitoJUnitRunner.class)
-public class TestFlowOrchestrator {
+public class TestFlowOrchestratorUsers {
 	
 	private FlowOrchestrator flowOrchestrator;
 	
@@ -35,55 +31,54 @@ public class TestFlowOrchestrator {
 	private UsersIOHandler userIO;
 	
 	@Mock
-	private BillingIOHandler billingIO;
-	
-	@Mock
 	private OffersIOHandler offerIO;
 	
-	@Mock
-	private RoadsIOHandler roadsIO;
-	
-	@Mock
-	private IntegrityIOHandler integrityIOHandler;
 	
 	@Before
 	@Test
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
 		flowOrchestrator = new FlowOrchestrator();
-		Whitebox.setInternalState(flowOrchestrator, "userIO", userIO);
-		Whitebox.setInternalState(flowOrchestrator, "billingIO", billingIO);
-		Whitebox.setInternalState(flowOrchestrator, "offerIO", offerIO);
-		Whitebox.setInternalState(flowOrchestrator, "roadsIO", roadsIO);
-		Whitebox.setInternalState(flowOrchestrator, "integrityIOHandler", integrityIOHandler);
 		
+		Whitebox.setInternalState(flowOrchestrator, "userIO", userIO);
+		Whitebox.setInternalState(flowOrchestrator, "offerIO", offerIO);
 		
 		when(userIO.identifyUserByMailPlusPassword("noisette@mail.com", "fabulousnoisette")).thenReturn(true);
-		//when(userIO.isIdentified("noisette@mail.com")).thenReturn(true);
-
+		
+		assertEquals(Whitebox.getInternalState(flowOrchestrator, "offerIO"), offerIO);
+		assertEquals(Whitebox.getInternalState(flowOrchestrator, "userIO"), userIO);
 	}
+	
+	//------- USERS ENTRY POINTS
 	
 	@Test
 	public void testCreateUser() {		
 		String jsonString = "{\"event\": \"create-user\" , \"data\": {\"name\":\"Noisette\", \"mail\":\"noisette@mail.com\",\"phone\":\"0675767778\",\"password\":\"fabulousnoisette\"}}";
 		FlowOrchestrationResult res = flowOrchestrator.orchestrateUsersEntryPoint(jsonString);
+		
 		assertEquals(res.getHttpStatus(),HttpStatus.OK);
 		assertEquals(res.getContent(),"User created.\n");	
 	}
 	
 	@Test
-	public void testidentifyUser() {		
-		String badUser = "{\"event\": \"identify-user\" ,\"data\":{\"mail\":\"noisette@mail.com\",\"password\":\"!fabulousnoisette\"}}";
-		FlowOrchestrationResult resBad = flowOrchestrator.orchestrateUsersEntryPoint(badUser);
-		assertEquals(resBad.getHttpStatus(),HttpStatus.NOT_ACCEPTABLE);
-		assertEquals(resBad.getContent(),"Wrong identifiers ! Please retry.\n");	
-		
-		
+	public void testidentifyGoodUser() {	
 		String goodUser = "{\"event\": \"identify-user\" ,\"data\":{\"mail\":\"noisette@mail.com\",\"password\":\"fabulousnoisette\"}}";
 		FlowOrchestrationResult resGood = flowOrchestrator.orchestrateUsersEntryPoint(goodUser);
+		
 		assertEquals(resGood.getHttpStatus(),HttpStatus.OK);
 		assertEquals(resGood.getContent(),"Successfully identified.\n");	
 	}
+	
+	
+	@Test
+	public void testidentifyBadUser() {		
+		String badUser = "{\"event\": \"identify-user\" ,\"data\":{\"mail\":\"noisette@mail.com\",\"password\":\"!fabulousnoisette\"}}";
+		FlowOrchestrationResult resBad = flowOrchestrator.orchestrateUsersEntryPoint(badUser);
+		
+		assertEquals(resBad.getHttpStatus(),HttpStatus.NOT_ACCEPTABLE);
+		assertEquals(resBad.getContent(),"Wrong identifiers ! Please retry.\n");	
+	}
+	
 	
 	@Test
 	public void testConsultUsers() {	
@@ -94,9 +89,11 @@ public class TestFlowOrchestrator {
 		
 		String jsonString = "{'event':'consult-users','data':{}}";
 		FlowOrchestrationResult res = flowOrchestrator.orchestrateUsersEntryPoint(jsonString);
+		
 		assertEquals(res.getHttpStatus(),HttpStatus.OK);
 		assertEquals(res.getContent(),users);	
 	}
+	
 	
 	@Test
 	public void testConsultUser() {	
@@ -106,17 +103,9 @@ public class TestFlowOrchestrator {
 		
 		String jsonString = "{'event':'consult-user','data':{'mail':'noisette@mail.com'}}";
 		FlowOrchestrationResult res = flowOrchestrator.orchestrateUsersEntryPoint(jsonString);
+		
 		assertEquals(res.getHttpStatus(),HttpStatus.OK);
 		assertEquals(res.getContent(),noisette);	
 	}
 	
-	
-	
-	
-
-
-
-
-
-
 }
