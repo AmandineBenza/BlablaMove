1ff31e82b6641126ad94919eb7b03d7a736567c6
Amandine Benza <amandine.benza@etu.unice.fr>
2019-02-01 10:12:02 +0100

    #19 pom.xml Spring Security

--- src/main/java/com/xaamruda/bbm/commons/communication/NetworkUtils.java
+++ src/main/java/com/xaamruda/bbm/commons/communication/NetworkUtils.java
@@ -28,4 +28,11 @@ public final class NetworkUtils {
 	    return ip;
 	}
 	
+	public static String getClientIP(HttpServletRequest request) {
+	    String xfHeader = request.getHeader("X-Forwarded-For");
+	    if (xfHeader == null){
+	        return request.getRemoteAddr();
+	    }
+	    return xfHeader.split(",")[0];
+	}
 }
