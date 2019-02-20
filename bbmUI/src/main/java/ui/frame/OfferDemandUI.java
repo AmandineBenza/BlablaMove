48c623220045f51ad0926ef39b6b8c228e0c761c
MEERSMAN Rudy <roody06@hotmail.fr>
2019-02-20 15:01:07 +0100

    #13 update ui
    little update for what he received

--- bbmUI/src/main/java/ui/frame/OfferDemandUI.java
+++ bbmUI/src/main/java/ui/frame/OfferDemandUI.java
@@ -1,5 +1,9 @@
 package ui.frame;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+
 import javax.swing.*;
 import java.io.*;
 import java.net.HttpURLConnection;
@@ -256,25 +260,36 @@ public class OfferDemandUI extends JFrame implements IGlobalUI {
     }
 
     private void setResponses(String data){
-        String[] res = {};
-        
-        String[] data1 = {"056402864","61","15","10/02/2019","0125678","125","Sophia","Nice"};
+        if(data.equals("[ ]\n")){
+            response.clear();
+        }else {
+        /*String[] data1 = {"056402864","61","15","10/02/2019","0125678","125","Sophia","Nice"};
         String[] data2 = {"045408964","42","12","10/02/2019","0167954","100","Sophia","Nice"};
         String[] data3 = {"026076584","12","10","10/02/2019","0236125","200","Sophia","Nice"};
         response.add(data1);
         response.add(data2);
-        response.add(data3);
-        response.add(res);
+        response.add(data3);*/
+            final GsonBuilder builder = new GsonBuilder();
+            final Gson gson = builder.create();
+            
+            String res = gson.toJson(data);
+            response.add(gson.toJson(data));
+        }
+
     }
 
     private void acceptButtonActionPerformed(java.awt.event.ActionEvent evt) {
         if(curlAction()) {
         //if(true){
-            acceptButton.setSelected(false);
-            frame.dispose();
-            //setResponses("");
-            //String[] newData = {"o2501191000",this.maximumPointSpendField.getText(),"25/01/19",this.startLocationField.getText(),this.arrivalLocationField.getText()};
-            new ShowOfferUI(connectedUser,response);
+            if(this.response.size() < 1){
+                JOptionPane.showMessageDialog(frame, "your request has no match");
+            }else {
+                acceptButton.setSelected(false);
+                frame.dispose();
+                //setResponses("");
+                //String[] newData = {"o2501191000",this.maximumPointSpendField.getText(),"25/01/19",this.startLocationField.getText(),this.arrivalLocationField.getText()};
+                new ShowOfferUI(connectedUser, response);
+            }
         }else{
             JOptionPane.showMessageDialog(frame, "You didn't fill all informations.");
         }
